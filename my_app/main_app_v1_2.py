d50aa269941d941f4e4dd5923d65103965b33556
Christian Matos <cmatos@corp.atg>
2025-01-09 19:25:12 -0500

    test flake8 and mypy

---
+++ my_app/main_app_v1_2.py
@@ -0,0 +1,15 @@
+from typing import List, Any
+
+def process_data(data):
+    return data
+def concatenate_strings(strings: List[str]):
+    return ''.join(strings)
+
+
+def main():
+    data = process_data("example")
+    print(data)
+
+
+if __name__ == "__name__":
+    main()
